c1088a82dff17e2f7ba2991af72aaa7999fef65a
EMMANUEL-JOTHAM77 <emmanuelavwerosuoghene@gmail.com>
2022-09-16 16:58:45 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,12 @@
-#!/bin/bash
-#include "main.h"
-#include <stdio.h>
+#include"main.h"
+#include<stdio.h>
 
 /**
  * print_most_numbers - checks for a digit (0 through 9).
  *
  * Return: Always 0.
  */
+
 void print_most_numbers(void)
 {
 	int c;
